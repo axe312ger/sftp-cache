5744b57f6407ccded6254a029831f5821b9f321a
Benedikt Rötsch <opensource@axe312.dev>
2020-05-25 23:28:55 +0200

    mock fast-glob instead of relying on local dir

--- src/filesystem.test.js
+++ src/filesystem.test.js
@@ -1,9 +1,29 @@
 const { resolve } = require('path')
+const fg = require('fast-glob')
 
 const { getLocalFiles } = require('./filesystem')
 
+jest.mock('fast-glob')
+
 test('parses local directory', async () => {
-  const root = resolve(__dirname, '..', '__tests__', 'fixtures')
+  fg.mockImplementation(() => [
+    {
+      name: 'example-file-name.md',
+      path: '/tmp/dir-to-cache/example-file-name.md',
+      stats: { size: 4312, mtime: 1234 }
+    },
+    {
+      name: 'file-in-root-dir.empty',
+      path: '/tmp/dir-to-cache/file-in-root-dir.empty',
+      stats: { size: 4312, mtime: 1234 }
+    },
+    {
+      name: 'file-with-content.md',
+      path: '/tmp/dir-to-cache/subdir/file-with-content.md',
+      stats: { size: 4312, mtime: 1234 }
+    }
+  ])
+  const root = resolve('/tmp')
   const dir = resolve(root, 'dir-to-cache')
   const tree = await getLocalFiles(dir, root)
 
