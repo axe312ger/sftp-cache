3c1912282e6b9cfc2fd86cd8fd890308a7e40a0e
Benedikt Rötsch <opensource@axe312.dev>
2021-01-27 17:41:45 +0100

    feat: add retry and timeout to file caching

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { NodeSSH } = require('node-ssh')
 const { default: PQueue } = require('p-queue')
 
 const diff = require('./diff')
-const { getRemoteFiles } = require('./sftp')
+const { getRemoteFiles, cacheFile, TimeoutError } = require('./sftp')
 const { getLocalFiles } = require('./filesystem')
 
 // Array of files to map of files by relative path in cache
@@ -52,26 +52,42 @@ async function syncDir({
     await Promise.all(
       filesToUpload.map((path) =>
         queue.add(async () => {
-          console.log(`Caching ${path}`)
-          const localPath = join(localCacheDir, path)
-          const remotePath = join(remoteCacheDir, path)
-          await ssh.putFile(localPath, remotePath)
-          const { mtime } = localMap[path].stats
-          await new Promise((resolve, reject) => {
-            sftp.setstat(
-              remotePath,
-              {
-                mtime,
-                atime: mtime
-              },
-              (err) => {
-                if (err) {
-                  return reject(err)
-                }
-                resolve()
+          console.log(`Start caching ${path}`)
+          const stats = localMap[path].stats
+          let tries = 0
+          let cached = false
+
+          while (!cached) {
+            tries++
+
+            try {
+              await cacheFile({
+                ssh,
+                sftp,
+                path,
+                stats,
+                localCacheDir,
+                remoteCacheDir
+              })
+              cached = true
+            } catch (err) {
+              if (err instanceof TimeoutError) {
+                console.log(err.message)
+              } else {
+                console.error(err)
               }
-            )
-          })
+              if (tries === 5) {
+                try {
+                  const remotePath = join(remoteCacheDir, path)
+                  await ssh.exec('rm', [remotePath])
+                } catch (err) {
+                  console.log('Unable to remove unfinished file fragment')
+                }
+                throw new Error(`Unable to cache ${path} in 5 attempts.`)
+              }
+              console.log(`Retry #${tries + 1}: Start caching ${path}`)
+            }
+          }
         })
       )
     )
