7ebcd813b1e0fc41830a91a7e5ac71744d9dd473
Benedikt Rötsch <opensource@axe312.dev>
2021-01-27 17:41:45 +0100

    feat: add retry and timeout to file caching

--- src/sftp.js
+++ src/sftp.js
@@ -96,9 +96,88 @@ async function getRemoteMd5({ ssh, path }) {
   return md5
 }
 
+class TimeoutError extends Error {
+  constructor(timeout, path, ...params) {
+    const seconds = timeout / 1000
+
+    super(
+      `Network issue: Aborting upload of ${path} after ${seconds.toFixed(
+        2
+      )} seconds`,
+      ...params
+    )
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, TimeoutError)
+    }
+
+    this.name = 'TimeoutError'
+    this.path = path
+  }
+}
+
+async function cacheFile({
+  ssh,
+  sftp,
+  path,
+  stats,
+  localCacheDir,
+  remoteCacheDir
+}) {
+  const { mtime, size } = stats
+
+  const localPath = join(localCacheDir, path)
+  const remotePath = join(remoteCacheDir, path)
+
+  // 10 seconds plus theoretical upload time with a 5mbit connection
+  const timeout = Math.ceil(
+    10 * 1000 + ((size * 8) / (5 * Math.pow(10, 6))) * 1000
+  )
+  let uploaded = false
+
+  const cacheFile = async (localPath, remotePath) => {
+    await ssh.putFile(localPath, remotePath, null, {
+      // @todo add code that aborts stalled uploads, should add extra performance on laggy connections
+      // step: (total_transferred, chunk, total) => {
+      //     console.log(path, 'uploaded', total_transferred, 'of', total);
+      // }
+    })
+    uploaded = true
+  }
+
+  await Promise.race([
+    cacheFile(localPath, remotePath),
+    new Promise((resolve, reject) =>
+      setTimeout(
+        () => !uploaded && reject(new TimeoutError(timeout, path)),
+        timeout
+      )
+    )
+  ])
+
+  await new Promise((resolve, reject) => {
+    sftp.setstat(
+      remotePath,
+      {
+        mtime,
+        atime: mtime
+      },
+      (err) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      }
+    )
+  })
+  console.log(`Cached ${path}`)
+}
+
 module.exports = {
   sftpMkdirP,
   sftpReadDir,
   getRemoteFiles,
-  getRemoteMd5
+  getRemoteMd5,
+  cacheFile,
+  TimeoutError
 }
